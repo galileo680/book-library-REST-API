18af74254e6dbdb033b22ba34c1c885900584188
galileo680 <bartek.wicz@gmail.com>
2023-09-01 11:40:39 +0200

    book adding/updating validation added'

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { body } = require('express-validator');
 
 const bookController = require('../controllers/bookController');
 const isAuth = require('../middleware/is-auth');
@@ -9,10 +10,30 @@ const router = express.Router();
 router.get('/books', bookController.getAllBooks);
 
 // /api/add-book => POST
-router.post('/add-book', isAuth, bookController.postAddBook);
+router.post(
+  '/add-book',
+  isAuth,
+  [
+    body('title').trim().isLength({ min: 4 }).not().isEmpty(),
+    body('author').trim().isLength({ min: 4 }).not().isEmpty(),
+    body('genre').trim().isLength({ min: 4 }).not().isEmpty(),
+    body('publicationYear').trim().isLength({ min: 4 }).not().isEmpty(),
+  ],
+  bookController.postAddBook
+);
 
 // /api/update-book => PUT
-router.put('/update-book/:bookId', isAuth, bookController.putUpdateBook);
+router.put(
+  '/update-book/:bookId',
+  isAuth,
+  [
+    body('title').trim().isLength({ min: 4 }).not().isEmpty(),
+    body('author').trim().isLength({ min: 4 }).not().isEmpty(),
+    body('genre').trim().isLength({ min: 4 }).not().isEmpty(),
+    body('publicationYear').trim().isLength({ min: 4 }).not().isEmpty(),
+  ],
+  bookController.putUpdateBook
+);
 
 // /api/delete-book => DELETE
 router.delete('/delete-book/:bookId', isAuth, bookController.deleteBook);
